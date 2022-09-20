a1b2795a3eb38d2c74c8b868b93790a5ec9a0d1b
Ghadeer-Issa92 <113966581+Ghadeer-Issa92@users.noreply.github.com>
2022-09-19 23:27:25 -0700

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,18 @@
+import inspect
+from time import time
+ 
+def timer_func(func):
+    def wrappper(*args, **kwargs):
+        wrappper.counter+=1
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        print(f'Function {func.__name__!r} execution time = {(end-start):.10f}s','called = ',wrappper.counter)
+        print(f'Source code:\n{inspect.getsource(func)}')
+        print('Docs :','\n', inspect.getdoc(func))
+        print('sign :','\n', inspect.signature(func))
+        print('pos_Arg :','\n', args)
+        print('kw_Arg :','\n', kwargs)
+        return result
+    wrappper.counter=0
+    return wrappper
